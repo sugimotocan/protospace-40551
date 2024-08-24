17de45bdbe36d311c451f0bdffbaa334619c3fce
sugimotocan <sugimotocan@gmail.com>
2024-08-24 10:41:12 +0900

    コメントを空のまま投稿すると詳細ページが再表示されるように修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,13 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
+      @comments = @prototype.comments.includes(:user)
       render 'prototypes/show'
     end
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,10 +21,8 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
-
   end
 
   def edit
